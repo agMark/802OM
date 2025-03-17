5aa561d4baad4f6bcc783082932ecd87239e33b4
Mark Nichols <mark.b.nichols@gmail.com>
2025-03-17 17:21:26 -0500

    added lubrication section

---
+++ DocDef_5_Repairs.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+
+let def_Sec_5 = new DocSection().i(true, "5", true, "REPAIRS", false, "", "page-break-before:always;");
+def_Sec_5.CustomClass = "breakBeforeRight";
---
+++ DocDef_6_AirworthinessLimitations.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let def_Sec_6 = new DocSection().i(true, "6", true, "AIRWORTHINESS LIMITATIONS", false, "", "page-break-before:always;");
+def_Sec_6.CustomClass = "breakBeforeRight";
